7772e92be4858abf7a4a7bac20aeba29b6ae449d
Kimp13 <akunec41@gmail.com>
2020-09-03 21:44:37 +0400

    Добавил .env, установил соединение с БД.

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
+require('dotenv').config();
+
 const Koa = require('koa'),
-      app = new Koa();
+      app = new Koa(),
+      knex = require('knex')({
+        client: process.env.DB_CLIENT,
+        connection: {
+          host: process.env.DB_HOST,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+          charset: process.env.DB_CHARSET
+        }
+      }),
+      bookshelf = require('bookshelf')(knex);
 
 app.use((ctx, next) => {
   const start = new Date();
   next().then(() => {
     const ms = Date.now() - start.getTime();
     console.log(
-      start.toISOString(),
-      '|',
-      ctx.method,
-      'on',
-      ctx.url,
-      'took',
-      ms,
-      'milliseconds'
+      `${start.toISOString()} | ${ctx.method} request on ${ctx.url}` +
+      'took ' + ms + ' milliseconds'
     );
   });
 });
